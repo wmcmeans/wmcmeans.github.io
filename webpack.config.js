0c6f3fb0c8b61e483022dad68f7af772c1fa27fb
William McMeans <wrmcmeans@gmail.com>
2016-02-13 16:23:18 -0500

    Set up files for writing portfolio

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  context: __dirname,
+  devtool: 'source-map'
+};
+module.exports = {
+  context: __dirname,
+  entry: "./lib/portfolio.js",
+  output: {
+    path: "./lib",
+    publicPath: "/lib/",
+    filename: "bundle.js",
+    devtoolModuleFilenameTemplate: '[resourcePath]',
+    devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
+  },
+  resolve: {
+    extensions: ["", ".js", ".jsx"]
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react']
+        }
+      },
+      {
+        test: /\.node$/,
+        loader: "node-loader"
+      }
+    ]
+  }
+};
